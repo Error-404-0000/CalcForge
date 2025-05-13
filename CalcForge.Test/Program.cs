5782a47d0a416297f55d72e9ec2a7ce98edce162
error <109008640+Error-404-0000@users.noreply.github.com>
2025-05-13 15:17:56 -0400

    Update Program.cs

--- CalcForge.Test/Program.cs
+++ CalcForge.Test/Program.cs
@@ -3,4 +3,4 @@ using CalcForge.Compiler;
 
 var e = new Evaluator("((((((2 + 0) + 3) * ((4 * 1) + (0 + 5))) + 2) / (((6 + 6) * 1) * (1 + 0 + 1))) +((100 / 5) - ((3 + 2) + 0))) +(((7 * 8) + (9 - 4 + 0)) * (((2 + 3 + 0) * 1) + 2)) -(((10 + 0 + 5) * 1) * ((6 - 0 - 1))) +((((50 + 50) / 2) * (((3 + 2) - 0) - (1 + 1)))) + 0 + 0 * 1234 ");
 Console.WriteLine(Compile.AsString(e.OptimizedTokens.ToArray(), "MASM64"));
-Compile.BuildFile(e.OptimizedTokens.ToArray(), "MASM64", ".asm", "C:\\Users\\Demon\\source\\repos\\CalcForge\\CalcForge.Test\\math");
+Compile.BuildFile(e.OptimizedTokens.ToArray(), "MASM64", ".asm", "CalcForge\\CalcForge.Test\\math");
